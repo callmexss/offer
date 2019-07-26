60960570c7f81aad4cf3cfea3e9380b645b1211e
callmexss <callmexss@126.com>
2019-07-26 18:06:27 +0800

    auto updates

--- src/netease/candy-puzzle.py
+++ src/netease/candy-puzzle.py
@@ -5,12 +5,26 @@ Email:        callmexss@126.com
 Github:       https://github.com/callmexss
 Description:  https://www.nowcoder.com/questionTerminal/8ff3e3a14ea04c6bb3a60e2e457dafb1
 """
-
 def process(info):
     info.sort()
+    count = 0
+    m = dict(zip(info, [0]*len(info)))
+    for each in info:
+        m[each] += 1
+    res = 0
+    for each in m.items():
+        val, num = each
+        while num > val:
+            res += val + 1
+            num = num - val - 1
+        if num == 0:
+            continue
+        else:
+            res += (val+1)
+    print(res)
 
 
 if __name__ == '__main__':
-    info = list(map(int, input().strip().split()))
+    info = list(map(int, input().split()))
     process(info)
 
